0d704a7332c3d51833438e7c320510f809369db2
lamery <lamery@ontech.com.au>
2010-08-24 12:31:40 +1000

    Added edit cartline

--- ShoppingCart/ShoppingCart.cs
+++ ShoppingCart/ShoppingCart.cs
@@ -75,6 +75,31 @@ namespace ezimerchant.Client
             return m_CartData;
         }
 
+        public IAsyncResult BeginEditCartLine(AsyncCallback callback, object state, int CartLineID, int Quantity)
+        {
+            if (m_AsyncResult != null)
+                throw new InvalidOperationException();
+
+            m_Dispatcher = Application.Current.RootVisual.Dispatcher;
+            m_Callback = callback;
+            m_CallbackState = state;
+
+            m_SignedFormData = "ACTION=UpdateCart&CARTLINE=" + CartLineID.ToString() + "&QUANTITY=" + Quantity.ToString();
+
+            var request = (HttpWebRequest)WebRequest.Create("https://" + SecureDomain + "/cart/?nocontent");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            m_AsyncResult = request.BeginGetRequestStream(RequestReady, request); 
+
+            return m_AsyncResult;
+        }
+
+        public void EndEditCartLine(IAsyncResult asyncResult0
+        {
+            if(m_AsyncResult != null)
+                m_AsyncResult.AsyncWaitHandle.WaitOne();
+        }
+
         private void FormReady(IAsyncResult asyncResult)
         {
             HttpWebRequest request = asyncResult.AsyncState as HttpWebRequest;
